1e849ae337e9220a4fbc493d3a46ce18b11acc88
KimH4nKyul <alskim93@kakao.com>
2025-12-26 17:08:38 +0900

    ♻️ Refactor: tuned fallback retries

--- src/fetcher/infrastructure/blockchain/blockchain.viem.client.ts
+++ src/fetcher/infrastructure/blockchain/blockchain.viem.client.ts
@@ -11,7 +11,6 @@ import {
 import { sepolia } from 'viem/chains';
 import { ConfigService } from '@nestjs/config';
 
-// TODO: infura API 호출 한도에 도달할 경우 Alchemy나 QuickNode API를 이용하도록 Failover를 설정해야 한다.
 @Injectable()
 export class BlockchainViemClient
   extends BlockchainClient
@@ -72,10 +71,6 @@ export class BlockchainViemClient
     }
   }
 
-  // viem은 짧은 시간(이벤트 루프의 한 틱) 동안 발생하는 여러 개의 요청을 감지해
-  // 자동으로 JSON-RPC Batch Request로 묶어 전송한다.
-  // 요청하려는 블록 범위(to - from)가 매우 클 경우, RPC 제공자가 요청이 너무 크다며 거부할 수 있다.
-  // TODO: batchSize 설정을 추가하여 안정성을 강화해야 한다. (선택: Nms 동안 요청을 모아 보내는 wait 옵션) => Infura에서 너무 많은 요청과 깊은 페이로드로 인해 차단
   public async fetchBlocks(from: bigint, to: bigint): Promise<Block[]> {
     const blocks: Block[] = [];
     for (let i = from; i <= to; i++) {
@@ -108,7 +103,7 @@ export class BlockchainViemClient
     const transports = rpcUrlList.map((url) =>
       http(url, {
         batch: { batchSize, wait: 100 },
-        retryCount: 2,
+        retryCount: 1, // 빠른 Failover를 위해 재시도 횟수 축소 (2 -> 1)
         retryDelay: 2000,
       }),
     );
